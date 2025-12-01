f97615e3c9be22b43ed18a5ec9a4ed01b6338ab2
7Bssk7 <arhip.fin3@gmail.com>
2025-11-30 18:32:31 -0800

    Update

--- 210-lab-36-starter.cpp
+++ 210-lab-36-starter.cpp
@@ -8,12 +8,21 @@ using namespace std;
 int main() {
 
     fstream inFile("codes.txt");
+    string code;
 
     if(!inFile){
         cout << "Failed to open the file!" << endl;
         inFile.close();
         return 1;
     }
+
+    while(getline(inFile, code)){
+        StrBinaryTree tree;
+
+        tree.insertNode(code);
+    }
+
+    
     
 
     return 0;
